f18e244cfa2cb76a751b01f3e06fa4a018c3a57f
NibnabCodes <hniamhh1995@gmail.com>
2026-05-01 15:39:18 +0000

    import db connections to function files

--- mysql_functions.py
+++ mysql_functions.py
@@ -1,3 +1,5 @@
 
+from db_connections import get_mysql_connection
+
 from colours import (print_header, print_error, 
                      print_data_row, print_success, print_info) 
--- recommendations.py
+++ recommendations.py
@@ -1,3 +1,5 @@
 
+from db_connections import get_mysql_connection, get_neo4j_driver
+
 from colours import (print_header, print_error, 
                      print_data_row, print_info)
